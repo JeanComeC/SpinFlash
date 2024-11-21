8af04a493dcac574a4b771e94aad98f47d1933ac
ParzivalPy <gabriel.arthus@icloud.com>
2024-11-21 21:47:06 +0100

    Fonctionnement du menu

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// affichage produits
+
 const hoverElement = document.querySelector(".prod");
+const hoverElementTel = document.querySelector(".prodTel");
 const targetDiv = document.querySelector(".produits");
 const removeElement = document.querySelector(".corps");
 
@@ -6,6 +9,25 @@ hoverElement.addEventListener("mouseenter", () => {
   targetDiv.style.display = "flex";
 });
 
+hoverElementTel.addEventListener("mouseenter", () => {
+  targetDiv.style.display = "flex";
+});
+
 removeElement.addEventListener("mouseenter", () => {
   targetDiv.style.display = "none";
+});
+
+// bouton menu version telephone
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.querySelector(".menu");
+  const liensTel = document.querySelector(".liensTel");
+
+  menuButton.addEventListener("click", () => {
+    if (liensTel.style.display === "none" || liensTel.style.display === "") {
+      liensTel.style.display = "flex";
+    } else {
+      liensTel.style.display = "none";
+    }
+  });
 });
