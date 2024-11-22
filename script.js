c689c8405e93ce9b41e8d3160ac6c4f6f36e2ac8
ParzivalPy <gabriel.arthus@icloud.com>
2024-11-22 09:02:37 +0100

    Ammélioration Navbar Animation

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 // affichage produits
+document.addEventListener("DOMContentLoaded", () => {
+  const hoverElement = document.querySelector(".prod");
+  const hoverElementTel = document.querySelector(".prodTel");
+  const targetDiv = document.querySelector(".produits");
+  const removeElement = document.querySelector(".corps");
 
-const hoverElement = document.querySelector(".prod");
-const hoverElementTel = document.querySelector(".prodTel");
-const targetDiv = document.querySelector(".produits");
-const removeElement = document.querySelector(".corps");
+  hoverElement.addEventListener("click", () => {
+    targetDiv.style.display = "flex";
+  });
 
-hoverElement.addEventListener("mouseenter", () => {
-  targetDiv.style.display = "flex";
-});
+  hoverElementTel.addEventListener("click", () => {
+    targetDiv.style.display = "flex";
+  });
 
-hoverElementTel.addEventListener("mouseenter", () => {
-  targetDiv.style.display = "flex";
-});
-
-removeElement.addEventListener("mouseenter", () => {
-  targetDiv.style.display = "none";
+  removeElement.addEventListener("mouseenter", () => {
+    targetDiv.style.display = "none";
+  });
 });
 
 // bouton menu version telephone
 
 document.addEventListener("DOMContentLoaded", () => {
-  const menuButton = document.querySelector(".menu");
+  const menuButton = document.querySelector("#toggleButton");
   const liensTel = document.querySelector(".liensTel");
+  const targetDiv = document.querySelector(".produits");
 
   menuButton.addEventListener("click", () => {
-    if (liensTel.style.display === "none" || liensTel.style.display === "") {
-      liensTel.style.display = "flex";
+    if (liensTel.style.height === "0" || liensTel.style.display === "") {
+      liensTel.style.height = liensTel.scrollHeight + "px";
+      liensTel.classList.add("expanded");
     } else {
-      liensTel.style.display = "none";
+      liensTel.style.height = "0";
+      liensTel.classList.remove("expanded");
     }
   });
 });
