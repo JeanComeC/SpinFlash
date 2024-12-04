9879ec49bca948e8890b618e77cc238c9820c143
ParzivalPy <gabriel.arthus@icloud.com>
2024-12-04 22:39:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// affichage produits
-// document.addEventListener("DOMContentLoaded", () => {
-//   const hoverElement = document.querySelector(".prod");
-//   const hoverElementTel = document.querySelector(".prodTel");
-//   const targetDiv = document.querySelector(".produits");
-//   const removeElement = document.querySelector(".corps");
-
-//   hoverElement.addEventListener("click", () => {
-//     targetDiv.style.display = "flex";
-//   });
-
-//   hoverElementTel.addEventListener("click", () => {
-//     targetDiv.style.display = "flex";
-//   });
-
-//   removeElement.addEventListener("mouseenter", () => {
-//     targetDiv.style.display = "none";
-//   });
-// });
-
 document.addEventListener("DOMContentLoaded", () => {
   const menuButton = document.querySelector("#toggleButton");
   const liensTel = document.querySelector(".liensTel");
